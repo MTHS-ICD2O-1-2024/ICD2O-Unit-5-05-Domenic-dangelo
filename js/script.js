a5aa44ece17456ed3df58beb9381bc9ffaed359d
Domenic <domenic.dangelo@stu.ocsb.ca>
2025-05-01 17:40:21 +0000

    javascript work

--- js/script.js
+++ js/script.js
@@ -11,51 +11,34 @@
  */
 // eslint-disable-next-line no-unused-vars
 function calculate() {
-  const lengthAString = prompt("sideA")
-  const lengthBString = prompt("sideB")
-  const lengthCString = prompt("sideC")
-
-  const lengthA = parseFloat(lengthAString)
-  const lengthB = parseFloat(lengthBString)
-  const lengthC = parseFloat(lengthCString)
+  const aLength = parseFloat(document.getElementById('a-length').value)
+  const bLength = parseFloat(document.getElementById('b-length').value)
+  const cLength = parseFloat(document.getElementById('c-length').value)
 
   // using the cosine law
-  const angleA = Math.acos((lengthB**2 + lengthC**2 - lengthA**2) / (2 * lengthB * lengthC)) * (180/Math.PI)
-  const angleB = Math.acos((lengthC**2 + lengthA**2 - lengthB**2) / (2 * lengthC * lengthA)) * (180/Math.PI)
-  const angleC = Math.acos((lengthA**2 + lengthB**2 - lengthC**2) / (2 * lengthA * lengthB)) * (180/Math.PI)
 
-  const sumOfAngles = Number((angleA).toFixed(2)) + Number((angleB).toFixed(2)) + Number((angleC).toFixed(2))
+const aAngle = Math.acos((bLength**2 + cLength**2 - aLength**2) / (2 * bLength * cLength)) * (180/Math.PI)
+const bAngle = Math.acos((cLength**2 + aLength**2 - bLength**2) / (2 * cLength * aLength)) * (180/Math.PI)
+const cAngle = Math.acos((aLength**2 + bLength**2 - cLength**2) / (2 * aLength * bLength)) * (180/Math.PI)
 
-  console.log(angleA)
-  console.log(angleB)
-  console.log(angleC)
-  console.log(sumOfAngles)
-  // input
-  const sideA = document.getElementById("sideA").value
-  const sideB = document.getElementById("sideB").value
-  const sideC = document.getElementById("sideC").value
+const sumOfAngles = Number((aAngle).toFixed(2)) + Number((bAngle).toFixed(2)) + Number((cAngle).toFixed(2))
 
-  // process
-  if (sumOfAngles === 180) {
-    if (sideA === sideB === sideC) {
-    // output
-    document.getElementById("result").innerHTML =
-      "<p>Your triangle is an equilateral triangle.</p>"
+  if (sumOfAngles == 180) {
+    if ((aAngle == bAngle) && (aAngle == cAngle)) {
+      document.getElementById("answer").innerHTML =
+      'This is an equilateral  triangle!'
+    } else if ((aAngle == bAngle) || (aAngle == cAngle) || (bAngle == cAngle)) {
+      document.getElementById("answer").innerHTML =
+      'This is an isosceles triangle!'
+    } else if ((aAngle != bAngle) || (aAngle != cAngle) || (bAngle != cAngle)) {
+      document.getElementById("answer").innerHTML =
+      'This is a scalene triangle!'
+    } else {
+      document.getElementById("answer").innerHTML =
+      'You should not be here!'
     }
-
-  else if(sideA === sideB || sideA === sideC || sideB === sideC){
-    document.getElementById("result").innerHTML =
-      "<p>Your triangle is an isosceles triangle.</p>"
-    }
-
-  else if(sideA != sideB || sideA != sideC || sideB != sideC){
-    document.getElementById("result").innerHTML =
-      "<p>Your triangle is an scalenea triangle.</p>"
-    }
-  }
-
-    else {
-    document.getElementById("result").innerHTML =
-      "<p>This is not a triangle.</p>"
+  } else {
+    document.getElementById("answer").innerHTML =
+    'This is not a triangle!'
   }
 }
